40c03f76ade22b3a49974b35feee1253184c8b5f
samwonghk <sam.wong@aspatial.com.hk>
2025-08-07 22:03:39 +0100

    Performance Improvement

--- src/components/CanvasComponent.vue
+++ src/components/CanvasComponent.vue
@@ -1,5 +1,8 @@
 <template>
-    <canvas ref="canvas"></canvas>
+    <div id="stage">
+        <canvas ref="canvas"></canvas>
+        <canvas ref="backgroundCanvas"></canvas>
+    </div>
 </template>
 
 <script>
@@ -7,13 +10,16 @@
         name: 'Canvas-Component',
         data() {
             return {
-                textStrings: [],
-                noOfTextStrings: 250,
-                noOfActiveTextStrings: 250,
-                fontSize: 12,
+                textStrings: [], // Array to hold text strings
+                noOfTextStrings: 200, // Number of text strings to generate
+                fontSize: 12, // Font size for text
                 fontColor: '#3a3', // Green color for text
-                fontFamily: 'monospace',
+                tailColor: '#333', // Color for the tail effect
+                highlightColor: '#3f3', // Color for highlighted text
+                fontFamily: 'monospace', // Font family for text
+                // letters to use for random text generation
                 letters: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789あアかカさサたタなナはハまマやヤらラわワいイきキしシちチにニひヒみミりリゐヰうウくクすスつツぬヌふフむムゆユるルえエけケせセてテねネへヘめメれレゑヱおオこコそソとトのノほホもモよヨろロをヲんンゃゅょ",
+                // Array of special messages to display
                 specialMessages: [
                     'Hello, World!',
                     'Welcome to Matrix Saver',
@@ -23,71 +29,31 @@
                     'Matrix Reloaded',
                     'Code is Poetry',
                     'Embrace the Matrix'
-                ]
+                ],
+                // Offscreen canvas for performance
+                offscreenCanvas: document.createElement('canvas'),
             };
         },
         mounted() {
-            this.initCanvas();
+            this.initCanvas(); // Initialize the canvas
+            // Generate initial text strings
             for (let i = 0; i < this.noOfTextStrings; i++) {
-                this.addTextString(this.generateText(), Math.random() * window.innerWidth, Math.random() * window.innerHeight, 2 + Math.random() * 10, Math.random() < 0.5);
+                this.addTextString(this.generateText(), Math.floor(Math.random() * window.innerWidth), Math.floor(Math.random() * window.innerHeight), Math.floor(1 + Math.random() * 5), Math.random() < 0.5);
             }
-            console.log(this.textStrings.map((textString) => textString.flipped).filter(flipped => flipped).length + ' flipped text strings');
+            // Set up offscreen canvas
+            this.offscreenCanvas.width = window.innerWidth;
+            this.offscreenCanvas.height = window.innerHeight;
+            
             let self = this;
-
-            function FrameRate(samples = 20) {
-                const times = [];
-                var s = samples;
-                while(s--) { times.push(0) }
-                var head = 0, total = 0, frame = 0, previouseNow = 0, rate = 0, dropped = 0;
-                const rates = [0, 10, 12, 15, 20, 30, 60, 90, 120, 144, 240];
-                const rateSet = rates.length;
-                const API = {
-                    sampleCount: samples,
-                    reset() {
-                        frame = total = head = 0;
-                        previouseNow = performance.now();
-                        times.fill(0);
-                    },
-                    set tick(soak) {
-                        const now = performance.now()
-                        total -= times[head];
-                        total += (times[head++] = now - previouseNow);
-                        head %= samples;
-                        frame ++;
-                        previouseNow = now
-                    },
-                    get rate() { return frame > samples ? 1000 / (total / samples) : 1 },
-                    get FPS() {
-                        var r = API.rate, rr = r | 0, i = 0;
-                        while (i < rateSet && rr > rates[i]) { i++ }
-                        rate = rates[i];
-                        dropped = Math.round((total - samples * (1000 / rate)) / (1000 / rate));
-                        return rate;
-                    },
-                    get dropped() { return dropped },
-                };
-                return API;
-            }
-
-            const fRate = FrameRate();
-            // var frame = 0;
-
-            fRate.reset();
+            // Start the animation loop
             function draw () {
-                // frame++;
-                fRate.tick = 1;
                 self.drawText();
-                // console.log('FPS:', fRate.FPS, 'Dropped frames:', fRate.dropped, 'Active TextStrings', self.textStrings.length);
-                if (isNaN(fRate.dropped) || fRate.dropped > 10) self.textStrings = self.textStrings.filter((text, index) => index < (self.textStrings.length / 2)); // Reduce text strings if too many frames are dro
-                // window.requestAnimationFrame(draw);
+                window.requestAnimationFrame(draw);
             }
-            setInterval(() => {
-                fRate.tick = 1;
-                draw();
-            }, 100); // 60 FPS
-            // window.requestAnimationFrame(draw);
+            window.requestAnimationFrame(draw);
         },
         methods: {
+            // Initialize the canvas
             initCanvas() {
                 const canvas = this.$refs.canvas;
                 if (canvas) {
@@ -95,10 +61,9 @@
                     canvas.width = window.innerWidth;
                     canvas.height = window.innerHeight;
                     ctx.fillStyle = '#000';
-                    ctx.fillRect(0, 0, canvas.width, canvas.height);
-                    // ctx.scale(-1, 1);
                 }
             },
+            // Add a new text string to the array
             addTextString(text, x, y, speed, flipped) {
                 const special = Math.random() < 0.1; // 10% chance to add a special message
                 if (special) {
@@ -107,22 +72,19 @@
                 }
                 this.textStrings.push({ text, x, y, speed, flipped });
             },
+            // Draw the text strings on the canvas
             drawText() {
                 const canvas = this.$refs.canvas;
                 if (canvas) {
-                    const ctx = canvas.getContext('2d');
-                    ctx.beginPath();
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    ctx.fillStyle = '#000';
-                    ctx.fillRect(0, 0, canvas.width, canvas.height);
+                    const ctx = this.offscreenCanvas.getContext('2d'); // Use offscreen canvas for performance
+                    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the offscreen canvas
                     ctx.fillStyle = this.fontColor; // Green color for text
                     this.textStrings.forEach((textString) => {
-                        // if (index >= this.noOfActiveTextStrings) return; // Limit the number of active text strings
                         ctx.font = this.fontSize + 'px ' + this.fontFamily;
                         ctx.scale(-1, 1)
                         if (textString.flipped) {
                             const gradient = ctx.createLinearGradient(textString.x, textString.y, textString.x + this.fontSize, textString.y + textString.text.length * this.fontSize);
-                            gradient.addColorStop(0, '#333'); // Green color for text
+                            gradient.addColorStop(0, this.tailColor); // Green color for text
                             gradient.addColorStop(1, this.fontColor); // Lighter green for gradient
                             ctx.fillStyle = gradient; // Apply gradient
                                 
@@ -131,7 +93,7 @@
                                 let text = char === ' ' ? ' ' : char; // Keep spaces as is
                                 text = Math.random() < 0.1 ? this.letters.charAt(Math.floor(Math.random() * this.letters.length)) : text; // Randomize characters
                                 if (index === textString.text.length - 1) {
-                                    ctx.fillStyle = '#3f3'; // Change color for the last character
+                                    ctx.fillStyle = this.highlightColor; // Change color for the last character
                                     ctx.fillText(text, -textString.x, textString.y + index * this.fontSize);
                                 } else {
                                     ctx.fillText(text, -textString.x, textString.y + index * this.fontSize);
@@ -141,32 +103,37 @@
                         } else {
                             ctx.font = this.fontSize + 'px ' + this.fontFamily;
                             const gradient = ctx.createLinearGradient(textString.x, textString.y, textString.x + this.fontSize, textString.y + textString.text.length * this.fontSize);
-                            gradient.addColorStop(0, '#333'); // Green color for text
+                            gradient.addColorStop(0, this.tailColor); // Green color for text
                             gradient.addColorStop(1, this.fontColor); // Lighter green for gradient
                             ctx.fillStyle = gradient; // Apply gradient
+
                             textString.text.split('').forEach((char, index) => {
                                 let text = char === ' ' ? ' ' : char; // Keep spaces as is
                                 text = Math.random() < 0.05 ? this.letters.charAt(Math.floor(Math.random() * this.letters.length)) : text; // Randomize characters
                                 if (index === textString.text.length - 1) {
-                                    ctx.fillStyle = '#3f3'; // Reset color for the next text string
+                                    ctx.fillStyle = this.highlightColor; // Reset color for the next text string
                                     ctx.fillText(text, textString.x, textString.y + index * this.fontSize);
                                 } else {
                                     ctx.fillText(text, textString.x, textString.y + index * this.fontSize);
                                 }
                             });
                         }
-                        textString.y += textString.speed; // Move text to the right
+
+                        textString.y += textString.speed; // Move text to the bottom
                         if (textString.y > canvas.height) {
                             textString.y = textString.text.length * -1 * this.fontSize; // Reset position if it goes off screen
-                            textString.x = Math.random() * canvas.width; // Randomize x position
+                            textString.x = Math.floor(Math.random() * canvas.width); // Randomize x position
                         }
                     });
-                    ctx.closePath();
+
+                    // Draw the offscreen canvas onto the main canvas
+                    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+                    canvas.getContext('2d').drawImage(this.offscreenCanvas, 0, 0);
                 }
             },
             generateText() {
                 let text = '';
-                let length = Math.floor(Math.random() * 30) + 5; // Random length between 1 and 10
+                let length = Math.floor(Math.random() * 25) + 5; // Random length between 1 and 10
                 for (let i = 0; i < length; i++) {
                     text += this.letters.charAt(Math.floor(Math.random() * this.letters.length));
                 }
@@ -187,5 +154,6 @@
         display: block; /* Remove inline-block spacing */
         width: 100vw; /* Full viewport width */
         height: 100vh; /* Full viewport height */
+        background: rgba(0, 0, 0, 255); /* Transparent background */
     }
 </style>
